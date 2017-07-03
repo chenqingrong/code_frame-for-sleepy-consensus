62911d4fd5baf77047c4796349a858796d7d129a
陈清荣 <chenqingrong@dhcp-mcfad-119.wifi-visitor.cit.cornell.edu>
2017-07-02 23:13:09 -0400

    header file

---
+++ header.h
@@ -0,0 +1,24 @@
+#ifndef header_h
+#define header_h
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+#include <string.h>
+using namespace std;
+struct msg
+{
+	int id;//who send this msg
+	int round;//expected time to send, only matters in attacker
+	int kind;//0-transaction, 1-block
+	int genround;//when this message is created
+	//more to be done on msg content,like send to who and what the content is
+};
+struct node
+{
+	int dept;//length of this chain
+	msg * content;// record the transaction
+	node * next;//the previous block can have multiple blocks as its next block
+	node * lchild; //next block
+	node(int dep=0,msg *con=NULL,node *ne=NULL,node * lch=NULL):dept(dep),content(con),next(ne),lchild(lch){}
+};
+#endif
